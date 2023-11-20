ae5d05c270f39c3b88a78e1f4d99846298344351
Naima Malik <noma_m@hotmail.com>
2023-11-20 22:46:13 +0100

    Toggling of completion of todos

--- src/components/TodoList/Todos.jsx
+++ src/components/TodoList/Todos.jsx
@@ -1,14 +1,45 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTodo } from "../../reducers/tasks";
+import styled from "styled-components";
+
+const StyledCheckbox = styled.input``;
 
 const Todos = () => {
   const todoList = useSelector((state) => state.tasks.todos);
+  const totalTasks = useSelector((state) => state.tasks.totalTasks);
+  const uncompletedTasks = useSelector((state) => state.tasks.uncompletedTasks);
+
+  const dispatch = useDispatch();
+
+  const handleToggle = (id) => {
+    dispatch(toggleTodo(id));
+  };
   return (
     <div>
       <h1>Todos</h1>
-      {todoList.map((list) => (
-        <ul key={list.id}>
-          <li>{list.text}</li>
-        </ul>
+      <p>{totalTasks} tasks</p>
+      <p>{uncompletedTasks} uncompleted tasks</p>
+
+      {todoList.map((todo) => (
+        <div key={todo.id}>
+          <ul>
+            <li>
+              <StyledCheckbox
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => handleToggle(todo.id)}
+              />
+              <span
+                style={{
+                  textDecoration: todo.complete ? "line-through" : "none",
+                }}
+              >
+                {todo.text}
+              </span>
+            </li>
+          </ul>
+        </div>
       ))}
     </div>
   );
--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -2,18 +2,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const todos = [
-  { id: 1, text: "Watch video on actions & reducers", complete: true },
-  { id: 2, text: "Follow redux codealong", complete: true },
-  { id: 3, text: "Fork weekly assignment", complete: true },
+  { id: 1, text: "Watch video on actions & reducers", complete: false },
+  { id: 2, text: "Follow redux codealong", complete: false },
+  { id: 3, text: "Fork weekly assignment", complete: false },
   { id: 4, text: "Create a todo app", complete: false },
 ];
 
 const initialState = {
   todos,
+  totalTasks: todos.length,
+  uncompletedTasks: todos.filter((todo) => !todo.complete).length,
 };
 
 export const tasks = createSlice({
   name: "tasks",
   initialState,
-  reducers: {},
+  reducers: {
+    // Toggle the completion status of a todo
+    toggleTodo: (state, action) => {
+      const todoToUpdate = state.todos.find(
+        (todo) => todo.id === action.payload
+      );
+      if (todoToUpdate) {
+        todoToUpdate.complete = !todoToUpdate.complete;
+        if (todoToUpdate.complete) {
+          state.uncompletedTasks--;
+        } else {
+          state.uncompletedTasks++;
+        }
+      }
+    },
+  },
 });
+
+export const { toggleTodo } = tasks.actions;
