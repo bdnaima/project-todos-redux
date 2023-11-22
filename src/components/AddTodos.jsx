0e58bebbf3524609e9f2b6b29b0b0363f09e93d3
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-11-22 10:27:57 +0100

    added date input task

--- src/components/AddTodos.jsx
+++ src/components/AddTodos.jsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { tasks } from "../reducers/tasks"
+import { useDispatch } from "react-redux";
+import { addTodo } from "../reducers/tasks";
 
 const AddTodo = () => {
-    const dispatch = useDispatch();
-    const [newTask, setNewTask] = useState("");
+  const dispatch = useDispatch();
+  const [newTask, setNewTask] = useState("");
+  const [date, setDate] = useState("");
 
-    const handleSubmitTodo = () => {
-        dispatch(tasks.actions.addTodo(newTask));
-        setNewTask("");
-    }
+  const handleSubmitTodo = () => {
+    dispatch(addTodo({ text: newTask, dueDate: date }));
+    setNewTask("");
+    setDate("");
+  };
 
-    return (
-        <>
-            <h1>Add todo</h1>
-            <input
-                type="text"
-                value={newTask}
-                onChange={(event) => setNewTask(event.target.value)} />
-            <button onClick={handleSubmitTodo}> Add task</button>
-        </>
-    )
-}
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="New task"
+        value={newTask}
+        onChange={(event) => setNewTask(event.target.value)}
+      />
+      <input
+        type="date"
+        value={date}
+        onChange={(event) => setDate(event.target.value)}
+      />
+
+      <button onClick={handleSubmitTodo}> Add task</button>
+    </>
+  );
+};
 
 export default AddTodo;
